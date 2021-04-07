5eaa0da561f887047895a27aae4ed1ec2eefc819
Diego Nogueira <diegoalvareznogueira@gmail.com>
2021-04-06 23:37:18 -0300

    added Docker support

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :skeleton_elasticsearch,
   repo: Skeleton.App.Repo,
   elasticsearch: Skeleton.App.Elasticsearch,
-  url: "http://localhost:9200",
+  url: "http://#{System.get_env("ELASTICSEARCH_HOST", "localhost")}:9200",
   refresh: true,
   namespace: :skeleton_elasticsearch,
   suffix: :test
@@ -15,8 +15,10 @@ config :skeleton_elasticsearch, ecto_repos: [Skeleton.App.Repo]
 config :skeleton_elasticsearch, Skeleton.App.Repo,
   adapter: Ecto.Adapters.Postgres,
   pool: Ecto.Adapters.SQL.Sandbox,
+  hostname: System.get_env("POSTGRES_HOST", "localhost"),
   database: "skeleton_elasticsearch_test",
-  username: System.get_env("SKELETON_ELASTICSEARCH_DB_USER") || System.get_env("USER") || "postgres"
+  password: System.get_env("POSTGRES_PASSWORD", "123456"),
+  username: System.get_env("POSTGRES_USERNAME") || System.get_env("USER") || "postgres"
 
 # Logger
 config :logger, :console, level: :error
