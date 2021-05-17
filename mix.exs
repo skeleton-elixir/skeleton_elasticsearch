5d215c62ed9475af9145ee38a457bfc3fa743ddf
Diego Nogueira <diegoalvareznogueira@gmail.com>
2021-05-17 17:31:00 +0000

    Release v1.2.3

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule SkeletonElasticsearch.MixProject do
   use Mix.Project
 
-  @version "1.2.2"
+  @version "1.2.3"
   @source_url "https://github.com/skeleton-elixir/skeleton_elasticsearch"
   @maintainers [
     "Diego Nogueira",
