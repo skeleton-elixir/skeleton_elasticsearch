6f03788f3853c6a2be5983609f453bf29024f58b
Diego Nogueira <diegoalvareznogueira@gmail.com>
2021-05-18 15:49:21 +0000

    Release v1.2.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+1.2.4
+------
+#### Changes
+* Get app name fixed
+
 1.2.3
 ------
 #### Changes
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule SkeletonElasticsearch.MixProject do
   use Mix.Project
 
-  @version "1.2.3"
+  @version "1.2.4"
   @source_url "https://github.com/skeleton-elixir/skeleton_elasticsearch"
   @maintainers [
     "Diego Nogueira",
