a8e17655b21d6c858795e48838df83d9b5cdfab8
Jhonathas Matos <jhonathas@gmail.com>
2022-05-21 11:54:23 -0300

    inform which modules will be indexed

--- lib/skeleton/elasticsearch/sync.ex
+++ lib/skeleton/elasticsearch/sync.ex
@@ -17,9 +17,36 @@ defmodule Skeleton.Elasticsearch.Sync do
       str_module = String.replace_prefix(to_string(sync_module), "Elixir.", "")
       Enum.empty?(sync_modules) || Enum.member?(sync_modules, str_module)
     end)
+    |> inform_which_modules_will_be_indexed(sync_modules)
     |> Enum.each(fn {sync_module, func, args} ->
       IO.puts("Start syncing #{sync_module}")
       apply(sync_module, func, args)
     end)
+  end
+
+  defp inform_which_modules_will_be_indexed(filtered_config_sync_modules, user_sync_modules) do
+    cond  do
+      filtered_config_sync_modules == [] and user_sync_modules != [] ->
+        IO.puts("""
+        \n
+        No indexers found with the names #{user_sync_modules}, you must pass the full name of the indexer module!
+
+        Example: MyApp.Accounts.UserIndexer
+        """)
+        IO.puts("")
+
+      filtered_config_sync_modules == [] ->
+        IO.puts("\nNo indexers found! You need to configure `sync_modules` in your Skeleton Elasticsearch settings\n")
+
+      true ->
+        module_names =
+          filtered_config_sync_modules
+          |> Enum.map(fn {module_name, _, _} -> module_name |> to_string() |> String.replace("Elixir.", "") end)
+          |> Enum.join(", ")
+
+        IO.puts("\nThe modules #{module_names} will be indexed!\n")
+    end
+
+    filtered_config_sync_modules
   end
 end
